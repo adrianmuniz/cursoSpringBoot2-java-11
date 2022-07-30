528e551109eb5972bc6b708f4f1a9c517e0e9c84
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-07-30 13:30:40 -0300

    User entity and Resources

---
+++ src/main/java/com/springweb/curso/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.springweb.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springweb.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999-9999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
